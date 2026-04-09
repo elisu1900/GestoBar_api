5bc76aff2570dd3c9975ffc1b548b9b5ff23b535
Elias <eliaswassit18@gmail.com>
2026-04-09 23:47:25 +0200

    initial commit: repository creation

---
+++ src/test/java/com/elias/GestoBar/GestoBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elias.GestoBar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
