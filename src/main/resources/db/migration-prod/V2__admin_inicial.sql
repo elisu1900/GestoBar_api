d9e61839085f8e83c3f764534c23defaee926b78
eliasVH <eliaswassit18@gmail.com>
2026-05-22 20:05:57 +0200

    fix: add deployment aplication-prod.yml, and a db with the user admin only

---
+++ src/main/resources/db/migration-prod/V2__admin_inicial.sql
@@ -0,0 +1,8 @@
+INSERT INTO gestobar.users (name, last_name, password, role, is_active)
+VALUES (
+           'Admin',
+           'Sistema',
+           '$2a$10$clBr2Im4kSygYIxjOPyKE.ZM94s7TAz2Dy4ATlfZ6YPXi5EzCV2Ny',
+           'ADMIN',
+           TRUE
+       );
